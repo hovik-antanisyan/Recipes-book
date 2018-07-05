75ebf9fd506afb2ea31ab7cd95310cfab5f20438
Victoriamazmanyan <victoriama1984.m@gmail.com>
2018-07-05 18:22:55 +0400

    Initialize ngrx bug fix

--- src/app/shared/state/shopping-list.actions.ts
+++ src/app/shared/state/shopping-list.actions.ts
@@ -6,7 +6,7 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export class AddIngredient implements Action {
     readonly type = ADD_INGREDIENT;
 
-    constructor(public payload: Ingredient[]) {console.log(this.payload);}
+    constructor(public payload: Ingredient) {}
 }
 
 export type ShoppingListActions = AddIngredient;
